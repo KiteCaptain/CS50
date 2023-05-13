b382022703abc06d46f1b6f2d792e71569570bd4
Milton Ngala <incaptain703@gmail.com>
2023-05-13 19:56:33 +0300

    stack overflow

---
+++ copy.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+   char *s = get_string("s: ");
+//    t[0] = toupper(t[0]);
+//    printf("s: %s\n", s);
+//    printf("t: %s\n", t);
+
+    char *t = malloc(strlen(s) + 1);
+    if (t == NULL)
+    {
+        return 1;
+    }
+
+    strcpy(t, s);
+    // for (int i = 0, n = strlen(s); i <= n; i++)
+    // {
+    //     // t[i] = s[i];
+    //     *(t + i) = *(s + i);
+    // }
+
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+
+    free(t);
+}
